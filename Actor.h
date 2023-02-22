1a657a7195239bb636a734cb4b9c47625ece341b
quirkycari <126111005+quirkycari@users.noreply.github.com>
2023-02-22 11:41:34 -0800

    Create Actor.h

---
+++ Actor.h
@@ -0,0 +1,85 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+class StudentWorld;
+
+
+//actor - derived from GraphObject
+class Actor: public GraphObject{
+    public:
+    private:
+};
+
+//players - peach & yoshi
+class Player_Avatar: public Actor{
+    
+};
+
+//Peach
+class Peach: public Player_Avatar{
+    
+};
+
+//Yoshi
+class Yoshi: public Player_Avatar{
+    
+};
+
+//baddies - boo & bowser
+class Baddies: public Actor{
+    
+};
+
+//Boo
+class Boo: public Baddies{
+    
+};
+
+//Bowser
+class Bowser: public Baddies{
+    
+};
+
+//squares - coin square, star square, directional square, bank square, event square, dropping square
+class Square: public Actor{
+    
+};
+
+//coin square
+class coinSquare: public Square{
+    
+};
+
+//star square
+class starSquare: public Square{
+    
+};
+
+//directional square
+class directionalSquare: public Square{
+    
+};
+
+//bank square
+class bankSquare: public Square{
+    
+};
+
+//event square
+class eventSquare: public Square{
+    
+};
+
+//dropping square
+class droppingSquare: public Square{
+    
+};
+
+
+//split it up into players (Peach & Yoshi), baddies (Boo & Bowser), squares, and vortex
+
+#endif // ACTOR_H_
