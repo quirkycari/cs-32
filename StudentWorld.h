8b0085b4787793cfee8645eb5b7a51f9de071e93
quirkycari <126111005+quirkycari@users.noreply.github.com>
2023-02-23 23:19:48 -0800

    Create StudentWorld.h

---
+++ StudentWorld.h
@@ -0,0 +1,32 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "Board.h"
+#include "Actor.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+  StudentWorld(std::string assetPath);
+  virtual int init();
+  virtual int move();
+  virtual void cleanUp();
+    ~StudentWorld();
+    StudentWorld();
+    
+ 
+    
+
+private:
+    Peach* m_peach;
+    vector<Actor*> actors;
+};
+
+#endif // STUDENTWORLD_H_
